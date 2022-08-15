94a1cf754e11fa96551ccbc5989bcf8744729600
Mark Tellez <mark@devmentor.live>
2022-08-15 15:18:24 -0500

    can pad6 and writeLine

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const input = [-4, 3, -9, 0, 4, 1];
 
-process.stdout.write("Hi\n");
+function pad6(float) {
+  return parseFloat(float).toFixed(6).toString();
+}
+
+function writeLine(str) {
+  process.stdout.write(str + "\n");
+}
+
+writeLine(pad6(0.5));
